986d9fdb74ee4b13137866f791fe5d6a7b76220e
zhangzrjerry <zzhangje@connect.ust.hk>
2026-04-16 23:54:22 +0800

    feat: add GitHub Actions workflow for deploying Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://zenithrobo.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
